339d77f40ca3438c4e193f75066bd9b8bef3006a
Scott Payton <spayton.c@gmail.com>
2019-09-16 10:20:55 -0600

    pending spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -9,20 +9,20 @@ RSpec.describe Course do
     it {should validate_presence_of :name}
   end
 
-  # describe "course_students_by_grade" do
-  #   it "returns students ordered by grade for specific class" do
-  #     scott = Student.create!(name: "Scott")
-  #     barb = Student.create!(name: "Barbara")
-  #     will = Student.create!(name: "William")
-  #     jim = Student.create!(name: "Jimbo")
-  #     math = Course.create!(name: "Math")
-  #     barb_courses = CoursesStudent.create!(student_id: barb.id, course_id: math.id, grade: 85)
-  #     jim_courses = CoursesStudent.create!(student_id: jim.id, course_id: math.id, grade: 3)
-  #     will_courses = CoursesStudent.create!(student_id: will.id, course_id: math.id, grade: 75)
-  #     scott_courses = CoursesStudent.create!(student_id: scott.id, course_id: math.id, grade: 95)
-  #
-  #     expect(math.course_students_by_grade).to eq([scott_courses,barb_courses,will_courses,jim_courses])
-  #   end
-  # end
+  describe "course_students_by_grade" do
+    xit "returns students ordered by grade for specific class" do
+      scott = Student.create!(name: "Scott")
+      barb = Student.create!(name: "Barbara")
+      will = Student.create!(name: "William")
+      jim = Student.create!(name: "Jimbo")
+      math = Course.create!(name: "Math")
+      barb_courses = CoursesStudent.create!(student_id: barb.id, course_id: math.id, grade: 85)
+      jim_courses = CoursesStudent.create!(student_id: jim.id, course_id: math.id, grade: 3)
+      will_courses = CoursesStudent.create!(student_id: will.id, course_id: math.id, grade: 75)
+      scott_courses = CoursesStudent.create!(student_id: scott.id, course_id: math.id, grade: 95)
+
+      expect(math.course_students_by_grade).to eq([scott_courses,barb_courses,will_courses,jim_courses])
+    end
+  end
 
 end
