6c31fdeac0bbf3a2fec1b8e4b65c99bf91d7c05c
Scott Payton <spayton.c@gmail.com>
2019-09-17 11:59:58 -0600

    Minor refactoring

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,11 +7,16 @@ end
 def create
   desired_name = params[:name]
   @student = Student.all.detect{|student| desired_name == student.name}
-  @course = Course.all[1]
-  @student.courses_students.create(student_id: @student.id, course_id: @course.id, grade: 100)
+  a = @student.courses
+  b = Course.all
+  desired_course = (a-b) | (b-a)
+  #well, I did this ONLY because we are adding 1 course at a time
+  #AND it would only make sense to enroll a student in a course that a student was not already in.
+  #and because I cant remember the "correct" way to do it off the top of my head :( 
+  @course = desired_course
+  @student.courses_students.create(student_id: @student.id, course_id: desired_course[0].id, grade: 100)
   redirect_to "/students/#{@student.id}"
 end
-
 
 def destroy
   desired_name = params[:name]
@@ -21,4 +26,6 @@ def destroy
   @student.courses[0].destroy
 redirect_to "/students/#{@student.id}"
 end
+
+
 end
