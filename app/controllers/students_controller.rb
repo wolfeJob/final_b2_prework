29e7da6442d6a0a637047092e545a8a00776edd3
Scott Payton <spayton.c@gmail.com>
2019-09-17 10:18:49 -0600

    Created student updated spec for courses, created form in courses view, and patch route for courses controller update action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,14 @@ def show
   @student = Student.find(params[:id])
 end
 
+def update
+@student = Student.find(params[:id])
+@course = Course.find(params[:course_id])
+@student.courses_students.create(student_id: @student.id, course_id: @course.id, grade: 100)
+# pry
+redirect_to "/students/#{@student.id}"
+end
+
+
+
 end
---
+++ spec/features/courses/update_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+describe 'When I visit course show page I can enroll a student' do
+  before(:each) do
+    @scott = Student.create!(name: "Scott")
+    @math = Course.create!(name: "Math")
+    @art = Course.create!(name: "art")
+    @chemistry = Course.create!(name: "Chemistry")
+    @scott_courses = CoursesStudent.create!(student_id: @scott.id, course_id: @math.id, grade: 95 )
+    @scott_courses_2 = CoursesStudent.create!(student_id: @scott.id, course_id: @chemistry.id, grade: 99 )
+  end
+
+  it "I see a form that enrolls a student from that students first name" do
+    visit "/courses/#{@art.id}"
+
+    fill_in :name, with: "#{@scott.name}"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/students/#{@scott.id}")
+    expect(page).to have_content(@art.name)
+end
+end
